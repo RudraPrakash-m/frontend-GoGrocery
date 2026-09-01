46a466f2757cfe97d954029e6f950d0dc607c51e
RudraPrakash1605 <rudra.mallick@aashdit.com>
2026-09-01 14:54:05 +0530

    proxy set

--- src/services/api/axiosInstance.js
+++ src/services/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://backend-gogrocery.onrender.com/api';
+const baseURL = import.meta.env.VITE_API_BASE_URL || '/api';
 
 const axiosInstance = axios.create({
   baseURL,
