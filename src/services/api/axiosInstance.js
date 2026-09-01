d01f38c75fe3237fd80905e9526f4720b15f11f3
RudraPrakash1605 <rudra.mallick@aashdit.com>
2026-09-01 12:44:27 +0530

    production url added

--- src/services/api/axiosInstance.js
+++ src/services/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://backend-gogrocery.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL,
